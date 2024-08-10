8ee62ddff7cd5d067c45a938f4b63d4bc45157b2
brequet <requet.baptiste@gmail.com>
2024-08-10 11:35:56 +0200

    imported dropdown menu, yet to use it

--- frontend/src/lib/components/LogEntryComponent.svelte
+++ frontend/src/lib/components/LogEntryComponent.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   import type { LogEntry } from "../types/LogEntry";
+  import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
+  import { filters } from "$lib/stores/Filters.svelte";
 
   export let entry: LogEntry;
 
@@ -17,13 +19,33 @@
         return "text-gray-500";
     }
   }
+
+  function setAsStartDate() {
+    console.log("set as start date", entry.timestamp);
+    // filters.startDate = entry.timestamp;
+  }
 </script>
 
 <div
   class="grid grid-cols-[auto_auto_auto_1fr] gap-x-2 py-1 font-mono text-sm hover:bg-gray-200"
 >
   <span class="text-neutral-400 whitespace-nowrap">{entry.app_name}</span>
-  <span class="text-neutral-500 whitespace-nowrap">{entry.timestamp}</span>
+  <DropdownMenu.Root>
+    <DropdownMenu.Trigger>
+      <span
+        class="text-neutral-500 whitespace-nowrap hover:text-neutral-600 hover:font-bold hover:cursor-pointer"
+        >{entry.timestamp}
+      </span>
+    </DropdownMenu.Trigger>
+    <DropdownMenu.Content>
+      <DropdownMenu.Group>
+        <DropdownMenu.Item onclick={setAsStartDate}
+          >Set as start date</DropdownMenu.Item
+        >
+        <DropdownMenu.Item>Set as end date</DropdownMenu.Item>
+      </DropdownMenu.Group>
+    </DropdownMenu.Content>
+  </DropdownMenu.Root>
   <span class="whitespace-nowrap {getLevelColor(entry.level)}"
     >[{entry.level}]</span
   >
